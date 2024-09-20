fca4f1b4e387c1a89cefa1e64dbaa6879120ce2a
rajapriyan1421 <rajapriyan2391@gmail.com>
2024-09-20 10:43:07 +0530

    Create ContinuousCompoundInterest.java

---
+++ ContinuousCompoundInterest.java
@@ -0,0 +1,12 @@
+public class ContinuousCompoundInterest
+{
+public static void main(String[] args)
+{
+double p-Double.parseDouble(args[0]))
+double r-Double.parseDouble(args[1]);
+double t-Double.parseDouble(args[2]);
+double expResult-Hath.exp(r,t);
+double FinalAmount-p*expResult)
+System.out.println("Fina) Amount "Final Amount);
+}
+}
